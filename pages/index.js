43ee9853b59c255f821c31c3f7a5bb6cabd0fd9a
Brian Bancroft <hello@brianbancroft.ca>
2022-05-12 17:20:30 -0700

    Style hopmepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import { getAllPostsByFrontMatter } from "../lib/getAllPostsByFrontmatter";
 
 export default function Home(props) {
+  const { posts } = props;
+
   return (
     <>
       <Head>
@@ -11,15 +14,32 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>Home Page</main>
+      <main className="pt-4">
+        <ul>
+          {posts.map((post) => (
+            <Link
+              href={`/posts/writing/${post.slug}`}
+              key={`${post.frontMatter.title}-${post.frontMatter.date}`}
+            >
+              <a className="w-full">
+                <div className="flex justify-between  max-w-prose px-2 mx-auto">
+                  <div className="">{post.frontMatter.title}</div>
+                  <div className="">{post.frontMatter.date}</div>
+                </div>
+                <div className="max-w-prose flex justify-end mx-auto text-slate-500 italic">
+                  <p className="text-xs">{post.frontMatter.tags}</p>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </ul>
+      </main>
     </>
   );
 }
 
-export async function getStaticProps(context) {
-  const posts = await getAllPostsByFrontMatter("blog");
+export async function getStaticProps() {
+  const posts = await getAllPostsByFrontMatter("writing");
 
-  console.log("Posts ", posts);
-
-  return { props: {} };
+  return { props: { posts } };
 }
