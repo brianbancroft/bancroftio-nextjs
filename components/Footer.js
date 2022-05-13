19a53b635bcc160c7b65a8c72f8d1a1045f75813
Brian Bancroft <hello@brianbancroft.ca>
2022-05-12 17:06:03 -0700

    Implement tailwind and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,40 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Footer() {
+  const { route } = useRouter();
+
   return (
     <>
-      <footer>
+      <footer className="absolute bottom-0 w-full flex justify-around">
         <Link href="/">
-          <a>write</a>
+          <a className={`text-xl py-2 ${route === "/" ? "underline" : ""}`}>
+            home
+          </a>
         </Link>
         <Link href="/photos">
-          <a>photos</a>
+          <a
+            className={`text-xl py-2 ${route === "/photos" ? "underline" : ""}`}
+          >
+            photos
+          </a>
         </Link>
         <Link href="/snippets">
-          <a>snippets</a>
+          <a
+            className={`text-xl py-2 ${
+              route === "/snippets" ? "underline" : ""
+            }`}
+          >
+            snippets
+          </a>
         </Link>
         <Link href="/about">
-          <a>about</a>
+          <a
+            className={`text-xl py-2 ${route === "/about" ? "underline" : ""}`}
+          >
+            about
+          </a>
         </Link>
       </footer>
     </>
