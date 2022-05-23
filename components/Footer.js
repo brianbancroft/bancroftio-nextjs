6e4c6da8878838ddf64074e8cf9d2ab1322cb9d6
Brian Bancroft <hello@brianbancroft.ca>
2022-05-23 15:15:28 -0700

    Clean the footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,35 +7,41 @@ function Footer() {
 
   return (
     <>
-      <footer className="absolute bottom-0 w-full flex justify-around">
-        <Link href="/">
-          <a className={`text-xl py-2 ${route === "/" ? "underline" : ""}`}>
-            home
-          </a>
-        </Link>
-        <Link href="/photos">
-          <a
-            className={`text-xl py-2 ${route === "/photos" ? "underline" : ""}`}
-          >
-            photos
-          </a>
-        </Link>
-        <Link href="/snippets">
-          <a
-            className={`text-xl py-2 ${
-              route === "/snippets" ? "underline" : ""
-            }`}
-          >
-            snippets
-          </a>
-        </Link>
-        <Link href="/about">
-          <a
-            className={`text-xl py-2 ${route === "/about" ? "underline" : ""}`}
-          >
-            about
-          </a>
-        </Link>
+      <footer className="absolute bottom-0 w-full ">
+        <div className="flex justify-around max-w-prose mx-auto">
+          <Link href="/">
+            <a className={`text-2xl py-2 ${route === "/" ? "underline" : ""}`}>
+              home
+            </a>
+          </Link>
+          <Link href="/photos">
+            <a
+              className={`text-2xl py-2 ${
+                route === "/photos" ? "underline" : ""
+              }`}
+            >
+              photos
+            </a>
+          </Link>
+          <Link href="/snippets">
+            <a
+              className={`text-2xl py-2 ${
+                route === "/snippets" ? "underline" : ""
+              }`}
+            >
+              snippets
+            </a>
+          </Link>
+          <Link href="/about">
+            <a
+              className={`text-2xl py-2 ${
+                route === "/about" ? "underline" : ""
+              }`}
+            >
+              about
+            </a>
+          </Link>
+        </div>
       </footer>
     </>
   );
