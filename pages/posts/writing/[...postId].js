4455f5e45d13c649cb0de08dbf6963ced0c47155
Brian Bancroft <hello@brianbancroft.ca>
2022-05-23 16:42:10 -0700

    Load content

--- pages/posts/writing/[...postId].js
+++ pages/posts/writing/[...postId].js
@@ -1,15 +1,35 @@
 import React from "react";
-import { getAllPostsByFrontMatter } from "../../../lib/getAllPostsByFrontmatter";
+import Head from "next/head";
 
-function BlogPost() {
-  return <div>Blog Post!</div>;
+import { getAllPostsByFrontMatter } from "../../../lib/getAllPostsByFrontmatter";
+import { getPostBySlug } from "../../../lib/getPostBySlug";
+
+function BlogPost(props) {
+  const { metadata, body } = props;
+
+  return (
+    <>
+      <Head>
+        <title>{props?.metadata?.title} - Brian Bancroft</title>
+        <meta name="description" content="Blog, CV, Aide Memoire." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Blog Post!</div>
+    </>
+  );
 }
 
 export async function getStaticProps(context) {
-  console.log("Ctx ", context);
-  console.log("hello world");
+  const [postId] = context?.params?.postId;
 
-  return { props: {} };
+  if (!postId) throw new Error("Unknown path");
+
+  const { frontMatter: metadata, markdownBody: body } = await getPostBySlug(
+    "writing",
+    postId
+  );
+
+  return { props: { metadata, body } };
 }
 
 export async function getStaticPaths() {
