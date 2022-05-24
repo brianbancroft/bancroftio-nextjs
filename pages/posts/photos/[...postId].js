6da45a1a233ee4417b20e1c7a439c34ecfc5088c
Brian Bancroft <hello@brianbancroft.ca>
2022-05-23 17:30:37 -0700

    Update photos mdx

---
+++ pages/posts/photos/[...postId].js
@@ -0,0 +1,61 @@
+import React from "react";
+import Head from "next/head";
+
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+
+import SyntaxHighlighter from "../../../components/SyntaxHighlighter";
+
+import { getAllPostsByFrontMatter } from "../../../lib/getAllPostsByFrontmatter";
+import { getPostBySlug } from "../../../lib/getPostBySlug";
+
+function BlogPost(props) {
+  const { metadata, source, loaded } = props;
+
+  if (!props.loaded) return <>loading</>;
+
+  const components = {
+    pre: (stuff) => <SyntaxHighlighter {...stuff} />,
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{props?.metadata?.title} - Brian Bancroft</title>
+        <meta name="description" content="Blog, CV, Aide Memoire." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <article className="prose p-4 pb-16 mx-auto">
+        <MDXRemote {...source} components={components} />
+      </article>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const [postId] = context?.params?.postId;
+
+  if (!postId) throw new Error("Unknown path");
+
+  const { frontMatter: metadata, markdownBody: body } = await getPostBySlug(
+    "writing",
+    postId
+  );
+
+  const source = await serialize(body);
+
+  return { props: { metadata, source, loaded: true } };
+}
+
+export async function getStaticPaths() {
+  const postId = await (
+    await getAllPostsByFrontMatter("photos")
+  ).map((i) => i.slug);
+
+  return {
+    paths: [{ params: { postId } }],
+    fallback: true, // false or 'blocking'
+  };
+}
+
+export default BlogPost;
--- pages/posts/photos/[...post].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function BlogPost() {
-  return <div>photo Post!</div>;
-}
-
-export default BlogPost;
